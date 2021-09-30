25ad50c5292f86287fba70c5e160ea174a180fbb
vahermihkel <vahermihkel@gmail.com>
2021-09-30 20:20:21 +0300

    angular routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
