2d8ed0d04ff5ebd482f92028c9a63b3f52a150ff
vahermihkel <65609256+vahermihkel@users.noreply.github.com>
2021-10-11 16:55:39 +0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent }, 
-  { path: "ostukorv", component: CartComponent },
+  // path: 'localhost:4200', component: ./home/home.component.html
+                            // + .css kujunduse jaoks
+                            // + .ts dünaamilisuse jaoks
+  { path: '', component: HomeComponent },
+    // path: 'localhost:4200/ostukorv', component ./cart/cart.component
+  { path: 'ostukorv', component: CartComponent },
 ];
 
 @NgModule({
