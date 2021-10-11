e50e4d79d2a2973b46a108c98a18ce687d0426a0
vahermihkel <65609256+vahermihkel@users.noreply.github.com>
2021-10-11 17:01:08 +0300

    Add files via upload

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+          // koolon annab tüübi, ükskõik mis massiiv on tüübiks
+          // võrdusmärgiga annan väärtust
+  cartItems: any[] = [
+    {pealkiri: 'ese1', hind: '100'},  // 0
+    {pealkiri: 'ese2', hind: '200'}   // 1
+  ];
 
+  // kuvage cart.homponenht.HTML-s täpselt samamoodi 
+  // nagu home.component.html-s sai tehtud
+
+  // 7 minutit kirjutamiseks
+  // 17.00-17.15
+  
+  // 18.45
+  
   constructor() { }
 
   ngOnInit(): void {
